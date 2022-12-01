11210e8bfcc77b92a2427bdfba8017bd5fb0a56d
isinnur <isinngnyy@gmail.com>
2022-12-01 18:00:33 +0300

    add function

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ function changeCircleColor(color){
    const circle = document.querySelector(".circle");
    circle.style.background=color;
 }
+
+function toggleMenu(){
+    var menuToggle = document.querySelector('.toggle');
+    var navigation = document.querySelector('.navigation');
+    menuToggle.classList.toggle('active')
+    navigation.classList.toggle('active');
+}
